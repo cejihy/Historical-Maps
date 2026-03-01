8afa8400fbab5e9b82d0bcf2970023cf77f34f1a
cejihy <orimeaphy@outlook.com>
2026-03-01 20:24:40 +0800

    图标

---
+++ service-worker.js
@@ -0,0 +1,72 @@
+// 缓存名称
+const CACHE_NAME = 'history-map-app-v1';
+
+// 需要缓存的资源
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'app-icon.svg',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// 安装service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 处理网络请求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果找到缓存的资源，返回缓存
+        if (response) {
+          return response;
+        }
+        // 否则发起网络请求
+        return fetch(event.request).then(
+          (response) => {
+            // 检查响应是否有效
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // 克隆响应，因为响应流只能使用一次
+            const responseToCache = response.clone();
+            
+            // 将响应添加到缓存
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          }
+        );
+      })
+  );
+});
